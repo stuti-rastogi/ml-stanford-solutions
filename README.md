e1059c3aa802ec08aefa6bd24ef01e92b6ac7715
Stuti R. Rastogi <stuti.r.rastogi@gmail.com>
2018-02-19 12:49:47 -0800

    Added code and updated README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ml-stanford-solutions
+
+This repository contains all the solutions to the programming exercises of the Machine Learning course offered by Stanford on Coursera, taught by Prof. Andrew NG.
+
+The assignments have been coded in Octave (MATLAB).
+
+Following are the topics I learnt and coded for thise course:
+
+1. Linear Regression - Gradient descent, feature normalization, single and multiple variables
+2. Logistic Regression - regularised
+3. Multi-class Classification - one vs all classifier training and prediction
+4. Neural Networks - Forward and back propagation, sigmoid and regularised gradient
+5. Regularised Linear Regression - Polynomial feature mapping, cross-validation curve
+6. Support Vector Machines - Gaussian Kernel
+7. K-means clustering, PCA - Closest centroid means, principal component analysis
+8. Anamoly Detection, Recommender Systems - Collaborative filtering
+
+The assignment descriptions are included in the directory of each assignment.
---
+++ machine-learning-ex2/ex2/plotData.m
@@ -0,0 +1,38 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+positive = find(y == 1);
+negative = find(y == 0);
+
+%plot (X(positive, 1), X(positive, 2), '+', 'markerfacecolor', 'b', 'MarkerSize', 7);
+%plot (X(negative, 1), X(negative, 2), 'o', 'markerfacecolor', 'y', 'MarkerSize', 7);
+
+% Plot Examples
+plot(X(positive, 1), X(positive, 2), 'k+','LineWidth', 2, ...
+     'MarkerSize', 7);
+plot(X(negative, 1), X(negative, 2), 'ko', 'MarkerFaceColor', 'y', ...
+     'MarkerSize', 7);
+
+xlabel('Exam 1 score');
+ylabel('Exam 2 score');
+
+legend('Admitted', 'Not Admitted');
+
+% =========================================================================
+
+
+
+hold off;
+
+end
